0f4fa531f630874448c41b06b3d2e988a8bc5db4
ss0646 <>
2023-01-06 16:51:13 +0900

    つぶやき投稿とその一覧の閲覧機能

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 /**
@@ -39,6 +40,25 @@ public class Main extends HttpServlet {
 		} else {
 			request.getRequestDispatcher("/WEB-INF/jsp/main.jsp").forward(request, response);
 		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		
+		if (text != null && text.length() != 0) {
+			ServletContext application = getServletContext();
+			List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+			User loginUser = (User) request.getSession().getAttribute("loginUser");
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic logic = new PostMutterLogic();
+			logic.execute(mutter, mutterList);
+			application.setAttribute("mutterList", mutterList);
+		}
+		
+		request.getRequestDispatcher("/WEB-INF/jsp/main.jsp").forward(request, response);
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/main.jsp
+++ src/main/webapp/WEB-INF/jsp/main.jsp
@@ -1,8 +1,9 @@
-<%@page import="model.User"%>
+<%@page import="model.User, model.Mutter, java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     <%
     User loginUser = (User) session.getAttribute("loginUser");
+    List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
     %>
 <!DOCTYPE html>
 <html>
@@ -12,7 +13,16 @@
 </head>
 <body>
 <h1>つぶやき掲示板メイン</h1>
-<p><%= loginUser.getName() %>さん、ログイン中</p>
+<p><%= loginUser.getName() %>さん、ログイン中
 <a href="/mutter-example/Logout">ログアウト</a>
+</p>
+<p><a href="/mutter-example/Main">更新</a></p>
+<form action="/mutter-example/Main" method="post">
+<input type="text" name="text">
+<input type="submit" name="つぶやく">
+</form>
+<% for (Mutter mutter : mutterList) { %>
+	<p><%= mutter.getUserName() %>:<%= mutter.getText() %></p>
+<% } %>
 </body>
 </html>
